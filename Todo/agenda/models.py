f386dc1d874da26ba82d287d499a93ccf0a1065a
Levi <levi.pittman92@gmail.com>
2025-08-10 20:30:18 -0500

    Added Task and TaskGroup models.

--- Todo/agenda/models.py
+++ Todo/agenda/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class TaskGroup(models.Model):
+  
+  # Fields.
+  title = models.CharField("title", max_length=128)
+
+  # Relationships.
+  owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+
+class Task(models.Model):
+
+  # Fields.
+  title = models.CharField("title", max_length=128)
+  description = models.CharField("description", max_length=512)
+  due_date = models.DateField("due_date")
+  completion_status = models.BooleanField("completed")
+
+  # Relationships.
+  owner = models.ForeignKey(User, on_delete=models.CASCADE)
+  group = models.ForeignKey(TaskGroup, on_delete=models.DO_NOTHING, blank=True, null=True)
