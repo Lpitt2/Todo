0bcb88eb81e992a988e53827e9329eab49346a26
LPitt2 <levi.pittman92@gmail.com>
2025-10-27 10:51:16 -0500

    Added user icon to pages.

---
+++ Todo/agenda/templatetags/user.py
@@ -0,0 +1,23 @@
+from django import template
+from django.contrib.auth.models import User
+from hashlib import sha256
+
+
+register = template.Library()
+
+
+@register.inclusion_tag("agenda/user_icon.html")
+def user_avatar(user):
+  """Returns the address of the user's gravitar image."""
+
+  # Declare base url.
+  BASE_URL = "https://gravatar.com/avatar"
+
+  # Verify that the user is not null.
+  if (user == None):
+    return { 'url': f"{BASE_URL}", 'username': "" }
+
+  # Hash the user's email address.
+  email_hash = sha256(user.email.encode("utf-8"))
+
+  return { 'url': f"{BASE_URL}/{email_hash.hexdigest()}", 'username': user.username.capitalize() }
